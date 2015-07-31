d6f02299c69f5566e32f791f4aaaaa0e76f1c41d
jatuponp <jatuponp@gmail.com>
2015-07-31 09:12:47 +0700

    Delete mysql password

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=10.0.1.20;dbname=www57',
-    'username' => 'www57',
-    'password' => '1q2w3e4r',
+    'username' => '',
+    'password' => '',
     'charset' => 'utf8',
     'tablePrefix' => 'tbl_'
 ];
