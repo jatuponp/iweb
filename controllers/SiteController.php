61b4c0bc78f55f59e747d8561570746c01fae8f0
jatuponp <jatuponp@gmail.com>
2015-08-06 14:05:23 +0700

    - update admin theme

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -8,11 +8,11 @@ use yii\web\Controller;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
+use app\models\User;
 
-class SiteController extends Controller
-{
-    public function behaviors()
-    {
+class SiteController extends Controller {
+
+    public function behaviors() {
         return [
             'access' => [
                 'class' => AccessControl::className(),
@@ -34,8 +34,7 @@ class SiteController extends Controller
         ];
     }
 
-    public function actions()
-    {
+    public function actions() {
         return [
             'error' => [
                 'class' => 'yii\web\ErrorAction',
@@ -47,13 +46,20 @@ class SiteController extends Controller
         ];
     }
 
-    public function actionIndex()
-    {        
+    public function actionIndex() {
+        //echo \app\models\User::getPHash('153426');
+//        $user = new User();
+//        $user->username = 'webmaster';
+//        $user->email = 'jatuponp@gmail.com';
+//        $user->setPassword('153426');
+//        $user->generateAuthKey();
+//        if ($user->save()) {
+//            return $user;
+//        }
         return $this->render('index');
     }
 
-    public function actionLogin()
-    {
+    public function actionLogin() {
         if (!\Yii::$app->user->isGuest) {
             return $this->goHome();
         }
@@ -63,20 +69,19 @@ class SiteController extends Controller
             return $this->goBack();
         } else {
             return $this->render('login', [
-                'model' => $model,
+                        'model' => $model,
             ]);
+            
         }
     }
 
-    public function actionLogout()
-    {
+    public function actionLogout() {
         Yii::$app->user->logout();
 
         return $this->goHome();
     }
 
-    public function actionContact()
-    {
+    public function actionContact() {
         $model = new ContactForm();
         if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
             Yii::$app->session->setFlash('contactFormSubmitted');
@@ -84,13 +89,13 @@ class SiteController extends Controller
             return $this->refresh();
         } else {
             return $this->render('contact', [
-                'model' => $model,
+                        'model' => $model,
             ]);
         }
     }
 
-    public function actionAbout()
-    {
+    public function actionAbout() {
         return $this->render('about');
     }
+
 }
